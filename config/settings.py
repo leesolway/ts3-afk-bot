689128dd1463234138b090234e510840c150217b
Lee Solway <lee@DESKTOP-7IUKFS8>
2024-02-04 23:57:09 +0000

    Ensure that either BLACKLIST_CHANNEL_IDS or WHITELIST_CHANNEL_IDS is defined, but not both, to prevent conflicting configuration.

--- config/settings.py
+++ config/settings.py
@@ -6,9 +6,14 @@ SERVER_ID = get_env_var('SERVER_ID', default='1', var_type=int)
 QUERY_USERNAME = get_env_var('QUERY_USERNAME', required=True)
 QUERY_PASSWORD = get_env_var('QUERY_PASSWORD', required=True)
 
-AFK_CHANNEL_ID = get_env_var('AFK_CHANNEL_ID', default='2', var_type=int)
-BLACKLIST_CHANNEL_IDS = [int(cid) for cid in get_env_var('BLACKLIST_CHANNEL_IDS', default='', var_type=str).split(',')]
-WHITELIST_CHANNEL_IDS = [int(cid) for cid in get_env_var('WHITELIST_CHANNEL_IDS', default='', var_type=str).split(',')]
+AFK_CHANNEL_ID = get_env_var('AFK_CHANNEL_ID', var_type=int, required=True)
+BLACKLIST_CHANNEL_IDS = [int(cid) for cid in get_env_var('BLACKLIST_CHANNEL_IDS', default='', var_type=str).split(',') if cid]
+WHITELIST_CHANNEL_IDS = [int(cid) for cid in get_env_var('WHITELIST_CHANNEL_IDS', default='', var_type=str).split(',') if cid]
 
 MAX_IDLE_TIME = get_env_var('MAX_IDLE_TIME', default='1800000', var_type=int)  # 30 minutes in milliseconds
+
+# Check to ensure both BLACKLIST_CHANNEL_IDS and WHITELIST_CHANNEL_IDS are not defined simultaneously
+if BLACKLIST_CHANNEL_IDS and WHITELIST_CHANNEL_IDS:
+    raise ValueError("Both BLACKLIST_CHANNEL_IDS and WHITELIST_CHANNEL_IDS are defined. Please define only one.")
+
 
